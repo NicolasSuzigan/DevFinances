5ce5796dd4ba4d6cafa5c249fdd2f60561e02087
NicolasSuzigan <nicolas_suzigan@hotmail.com>
2021-07-22 23:42:20 -0300

    Aula 3.2

--- scripts.js
+++ scripts.js
@@ -17,13 +17,13 @@ const transactions = [
   },
   {
     id: 2,
-    description: 'luz',
-    amount: -50000,
+    description: 'internet',
+    amount: 50000,
     date: '23/01/2021',
   },
   {
     id: 3,
-    description: 'luz',
+    description: 'força',
     amount: -20000,
     date: '23/01/2021',
   },
@@ -40,3 +40,45 @@ const Transaction = {
 
   }
 }
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+      const tr = document.createElement('tr');
+      tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+  
+      DOM.transactionsContainer.appendChild(tr);
+
+    },
+
+  innerHTMLTransaction(transaction) {
+    //Operação para verificar se positivo ou negativo o lançamento
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    //
+    const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <th>
+            <img src="./assets/minus.svg" 
+            alt="Remover lançamento">
+        </th>
+      
+    `
+    return html;
+  }
+}
+
+const Utils = {
+    formatCurrency(value) {
+      const signal = number(value) < 0 ? "-" : " ";
+
+      value = String(value).replace()
+    }
+}
+
+transactions.forEach(function(transaction){
+  DOM.addTransaction(transaction);
+});
